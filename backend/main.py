58ae3b351ae0561ade5945b73bdce4e9a357fc9c
anushkasharma <aanushka.sharma@mail.utoronto.ca>
2024-06-13 07:46:03 -0400

    extract data and compare with baseline

---
+++ backend/main.py
@@ -0,0 +1,80 @@
+import json
+
+# Step 1: Upload the SARIF file (read from the local file system)
+file_path = 'db/database-a632c89dd778-2024-06-07.sarif'
+baseline_file_path = 'db/database-b8b8ebcf851d-2017-04-11.sarif'
+
+def extract_baseline():
+    '''
+    Extract baseline
+    :return: list contaning dictionaries for issues
+    '''
+    with open(baseline_file_path, 'r', encoding='utf-8') as f:
+        baseline_data = json.load(f)
+    issues = []
+    issues_full_list = baseline_data['runs'][0]['tool']['driver']['rules']
+    for issue in issues_full_list:
+        # print(issue)
+        prop = issue['properties']
+        if 'security' in prop['tags']: #gets all VULNERABILITIES
+            curr_issue = {}
+            curr_issue['id'] = issue['id']
+            curr_issue['name'] = issue['name']
+            curr_issue['shortDescription'] = issue['shortDescription']
+            curr_issue['fullDescription'] = issue['fullDescription']
+            curr_issue['security-severity'] = float(issue['properties']['security-severity'])
+            issues.append(curr_issue)
+    return issues
+
+def extract_new(new_file_path: str):
+    """
+    Extract user's file
+
+    :param new_file_path: user's file uploaded
+    :return:
+    """
+    with open(new_file_path, 'r', encoding='utf-8') as f:
+        baseline_data = json.load(f)
+    issues = []
+    issues_full_list = baseline_data['runs'][0]['tool']['driver']['rules']
+    for issue in issues_full_list:
+        # print(issue)
+        prop = issue['properties']
+        if 'security' in prop['tags']:  # gets all VULNERABILITIES
+            curr_issue = {}
+            curr_issue['id'] = issue['id']
+            curr_issue['name'] = issue['name']
+            curr_issue['shortDescription'] = issue['shortDescription']
+            curr_issue['fullDescription'] = issue['fullDescription']
+            curr_issue['security-severity'] = float(issue['properties']['security-severity'])
+            issues.append(curr_issue)
+    return issues
+
+
+def compare(baseline, new):
+    """
+
+    :param baseline:
+    :param new:
+    :return:
+    """
+    total_new_issues_in_new = len(baseline)-len(new)
+    new_issues = {}
+    ids=[]
+
+    for b in baseline:
+        ids.append(b['id'])
+
+    for n in new:
+        id = n['id']
+        if id not in ids:  # NEW ID FOUND --> NEW ISSUE, NOT IN BASELINE
+            new_issues[id] = n['security-severity']
+    print(new_issues)
+
+
+
+
+baseline = extract_baseline()
+new = extract_new(file_path)
+
+compare(baseline, new)
